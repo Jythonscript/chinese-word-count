e3c92904e49d9b0d0b312a9f9c22e9aa828925c1
Jythonscript <90302927@ep-student.org>
2019-04-10 14:21:50 -0500

    show matches and nonmatches when finished

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 import re
 
-#characters that do not count as a word
-notchars = "abcdefghijklmnopqrstuvwxyz"
 #number of chinese characters counted
 numchinese = 0
+#string full of matches
+matches = ""
+#string full of characters that did not match
+nonmatches = ""
 
 for line in open("input.txt", "r").readlines():
     for char in line:
-        if re.search(u'[\u4e00-\u9fff]', char):
+        if re.search(u'[\u4e00-\u9fff]', char):#if chinese
+            matches += char
             numchinese += 1
-            print(char,end='')
-print("\nChinese character count: " + str(numchinese))
+        else:#if not chinese
+            if char == '\n':
+                nonmatches += "\\n"
+            elif char == '\t':
+                nonmatches += "\\t"
+            else:
+                nonmatches += char
+
+print("Matches: '" + matches + "'")
+print("Nonmatches: '" + nonmatches + "'")
+print("Chinese character count: " + str(numchinese))
